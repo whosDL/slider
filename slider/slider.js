6bd573728e40c167d07a4cb2bd1f09d05dde7151
Demonly <demonly@users.noreply.github.com>
2016-10-19 11:07:27 +0800

    Completed responsive image
    Completed responsive image

--- slider/slider.js
+++ slider/slider.js
@@ -1,69 +1,80 @@
 var option = {
-	num:5,	//slider数量
-	sliderId:"slider",	//slider的id
-	sliderMedia:"sliderMedia",	//slider媒体查询的id
-	itemId:[],	//对应的物件盒子Id的前缀
-	basePath:"",	//slider图片的保存路径
-	switchTime:5000,	//slider切换时间
+    num:5,    //slider数量
+    sliderId:"slider",    //slider的id
+    sliderMedia:"sliderMedia",    //slider媒体查询的id
+    itemId:[],    //对应的物件盒子Id的前缀
+    basePath:"",    //slider图片的保存路径
+    switchTime:5000,    //slider切换时间
+}
+function throttle(method,context){
+    clearTimeout(method.tId);
+    method.tId=setTimeout(function(){
+        method.call(context);
+    },500);
 }
 
 var EventHandler = function(){
-	window.resize = function(){
-		if(media.testBreak()){
-			slider.refreshImg();
-		};
-	}
+    window.onresize = function(){
+        throttle(function(){
+                    if(media.testBreak()){
+                        slider.refreshImg();
+                    };
+                },window)
+    }
 };
 
 var Media = (function(){
-	var mediaCatcher = /@media\s*screen\s*and\s*(?:\(\s*((?:max)?(?:min)?)-width:(\d*)px\)\s*){0,3}\n?\s*\{\n?\s*\[(.*)\]\n?\s*\}/g;
-	var resolutions,difference;
+    var mediaCatcher = /@media\s*screen\s*and\s*(?:\(\s*((?:max)?(?:min)?)-width:(\d*)px\)\s*){0,3}\n?\s*\{\n?\s*\[(.*)\]\n?\s*\}/g;
+    var resolutions;
 
-	//构造函数
-	Media = function(){
-		//获取媒体查询
-		var MediaObj = document.getElementById(option.sliderMedia);
-		//解析媒体查询
-		var mediaText = MediaObj.innerHTML;
-		this.sliderMedia = [];
-		//循环获取每个媒体查询内容
-		while(resolutions = mediaCatcher.exec(mediaText)){
-			//创建断点对象
-			var breakPoint = new BreakPoint(resolutions[resolutions.length - 1]);
-			//写入max和min
-			switch(resolutions.length){
-				case 4:
-					breakPoint[resolutions[1]] = resolutions[2];
-					break;
-				case 6:
-					breakPoint[resolutions[1]] = resolutions[2];
-					breakPoint[resolutions[3]] = resolutions[4];
-					break;
-			}
-			//保存断点对象
-			this.sliderMedia.push(breakPoint);
-		}
-		//移除媒体查询对象
-		MediaObj.parentNode.removeChild(MediaObj);
-		//保存了与当前分辨率匹配的断点对象
-		this.curBreak = this.testBreak();
-	};
+    //构造函数
+    Media = function(){
+        //获取媒体查询
+        var MediaObj = document.getElementById(option.sliderMedia);
+        //解析媒体查询
+        var mediaText = MediaObj.innerHTML;
+        this.sliderMedia = [];
+        //循环获取每个媒体查询内容
+        while(resolutions = mediaCatcher.exec(mediaText)){
+            //创建断点对象
+            var breakPoint = new BreakPoint(resolutions[resolutions.length - 1]);
+            //写入max和min
+            switch(resolutions.length){
+                case 4:
+                    breakPoint[resolutions[1]] = resolutions[2];
+                    break;
+                case 6:
+                    breakPoint[resolutions[1]] = resolutions[2];
+                    breakPoint[resolutions[3]] = resolutions[4];
+                    break;
+            }
+            //保存断点对象
+            this.sliderMedia.push(breakPoint);
+        }
+        //移除媒体查询对象
+        MediaObj.parentNode.removeChild(MediaObj);
+        //保存了与当前分辨率匹配的断点对象
+        this.testBreak();
+    };
 
-	//返回与当前分辨率匹配的断点对象
-	Media.prototype.testBreak = function(){
-		this.Width = window.innerWidth|| document.documentElement.clientWidth|| document.body.clientWidth;
-		var hasChanged = false;
-		for (var i = 0; i < this.sliderMedia.length; i++) {
-			if (this.Width<this.sliderMedia[i].max && this.width>this.sliderMedia[i].min && difference > this.sliderMedia[i].max - this.sliderMedia[i].min) {
-				difference = this.sliderMedia[i].max - this.sliderMedia[i].min;
-				this.curBreak = this.sliderMedia[i];
-				hasChanged = true;
-			}
-		}
-		return hasChanged;
-	}
+    //返回与当前分辨率匹配的断点对象
+    Media.prototype.testBreak = function(){
+        this.width = document.documentElement.clientWidth|| document.body.clientWidth;
+        var hasChanged = false,difference = 3000,tempBreak;
+        for (var i = 0; i < this.sliderMedia.length; i++) {
+            if (this.width<=this.sliderMedia[i].max && this.width>=this.sliderMedia[i].min && difference>this.sliderMedia[i].max-this.sliderMedia[i].min) {
+                difference = this.sliderMedia[i].max - this.sliderMedia[i].min;
+                tempBreak = this.sliderMedia[i];
+            }
+        }
+        if(this.curBreak != tempBreak){
+            this.curBreak = tempBreak;
+            hasChanged = true;
+        }
+        return hasChanged;
+    }
 
-	return Media;
+    return Media;
 })();
 //这个对象中包含一些与option.sliderMedia中的项同名的媒体查询对象
 //每个媒体查询对象都有一系列断点对象
@@ -71,30 +82,33 @@ var Media = (function(){
 
 //断点对象构造函数
 var BreakPoint = function(images){
-	this.max = 3000;
-	this.min = 0;
-	this.imgs = images.split(",");
+    this.max = 3000;
+    this.min = 0;
+    this.imgs = images.split(",");
 };
 
 var Slider = (function(){
-	var sliderNode = document.getElementById(option.sliderId);
-	var sliderUl = sliderNode.getElementsByTagName("ul")[0];
-	var sliders = sliderUl.getElementsByTagName("li");
-	Slider = function(){
-		for (var i = 0; i < sliders.length; i++) {
-			var imgNode = document.createElement('img');
-			imgNode.src = option.basePath + curImg[i];
-			sliders[i].appendChild(imgNode);
-		}
-	};
+    var sliderNode = document.getElementById(option.sliderId);
+    var sliderUl = sliderNode.getElementsByTagName("ul")[0];
+    Slider = function(){
+        this.sliders = sliderUl.getElementsByTagName("li");
+        this.curImg = media.curBreak.imgs;
+        for (var i = 0; i < this.sliders.length; i++) {
+            var imgNode = document.createElement('img');
+            imgNode.setAttribute("src", option.basePath + this.curImg[i]);
+            this.sliders[i].appendChild(imgNode);
+        }
+        this.refreshImg();
+    };
 
-	//更新slider图片
-	Slider.prototype.refreshImg = function(){
-		for (var i = 0; i < sliders.length; i++) {
-			sliders[i].getElementsByTagName("img").src = basePath + curImg[i];
-		}
-	}
-	return Slider;
+    //更新slider图片
+    Slider.prototype.refreshImg = function(){
+        this.curImg = media.curBreak.imgs;
+        for (var i = 0; i < this.sliders.length; i++) {
+            this.sliders[i].getElementsByTagName("img")[0].setAttribute("src", option.basePath + this.curImg[i]);
+        }
+    }
+    return Slider;
 })();
 
 //实例化
